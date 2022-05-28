98d71ed300d28f8faa281d7d4eecb74e34001dd6
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-05-28 21:32:26 +0100

    added logic and put form in function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Form from "./Form";
 
-var userIsRegistered = false;
+var isLoggedIn = false;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    return (
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="container">
-      <Form />
-    </div>
-  );
+  return <div className="container">{renderConditionally}</div>;
 }
 
 export default App;
